60f18cc3a808a49ed6d5db4567b91d5555fb37b2
Glitch (glitch-hello-node) <none>
2023-09-05 13:51:50 +0000

    😇🍱 Checkpoint ./server.js:61168360/7171

--- server.js
+++ server.js
@@ -1,119 +1,143 @@
-/**
- * This is the main Node.js server script for your project
- * Check out the two endpoints this back-end API provides in fastify.get and fastify.post below
- */
+///////////////////////////COMEÇO DO CÓDIGO//////////////////////////////
 
-const path = require("path");
+const express = require("express");
+const app = express();
+const { WebhookClient } = require("dialogflow-fulfillment");
+const bodyParser = require("body-parser");
+const axios = require("axios");
 
-// Require the fastify framework and instantiate it
-const fastify = require("fastify")({
-  // Set this to true for detailed logging:
-  logger: false,
-});
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+  })
+);
+  //FUNÇÃO TAMEZONE
+  let date = new Date();
+  let data = date.toLocaleString("pt-BR", {timeZone: "America/Sao_Paulo", hour: "numeric", hour12: false}); 
 
-// ADD FAVORITES ARRAY VARIABLE FROM TODO HERE
-
-// Setup our static files
-fastify.register(require("@fastify/static"), {
-  root: path.join(__dirname, "public"),
-  prefix: "/", // optional: default '/'
-});
-
-// Formbody lets us parse incoming forms
-fastify.register(require("@fastify/formbody"));
-
-// View is a templating manager for fastify
-fastify.register(require("@fastify/view"), {
-  engine: {
-    handlebars: require("handlebars"),
-  },
-});
-
-// Load and parse SEO data
-const seo = require("./src/seo.json");
-if (seo.url === "glitch-default") {
-  seo.url = `https://${process.env.PROJECT_DOMAIN}.glitch.me`;
+  //FUNÇÃO CRIAR ID 
+  function uniqueID(){
+  function chr4(){
+    return Math.random().toString(16).slice(-4);
+  }
+  return chr4();
 }
 
-/**
- * Our home page route
- *
- * Returns src/pages/index.hbs with data built into it
- */
-fastify.get("/", function (request, reply) {
-  // params is an object we'll pass to our handlebars template
-  let params = { seo: seo };
-
-  // If someone clicked the option for a random color it'll be passed in the querystring
-  if (request.query.randomize) {
-    // We need to load our color data file, pick one at random, and add it to the params
-    const colors = require("./src/colors.json");
-    const allColors = Object.keys(colors);
-    let currentColor = allColors[(allColors.length * Math.random()) << 0];
-
-    // Add the color properties to the params object
-    params = {
-      color: colors[currentColor],
-      colorError: null,
-      seo: seo,
-    };
-  }
-
-  // The Handlebars code will be able to access the parameter values and build them into the page
-  return reply.view("/src/pages/index.hbs", params);
+app.use(express.static("public"));
+app.get("/", (request, response) => {
+  response.Text = "WEBHOOK - Fincionando no Glitch!";
 });
 
-/**
- * Our POST route to handle and react to form submissions
- *
- * Accepts body data indicating the user choice
- */
-fastify.post("/", function (request, reply) {
-  // Build the params object to pass to the template
-  let params = { seo: seo };
+app.post("/barbearia", function(request, response) {
+  const agent = new WebhookClient({
+    request: request,
+    response: response
+  });
 
-  // If the user submitted a color through the form it'll be passed here in the request body
-  let color = request.body.color;
+  let intentMap = new Map();
+  intentMap.set("Default Welcome Intent", welcome);
+  intentMap.set("Default Welcome Intent - no", cadastro);
+  intentMap.set("Default Welcome Intent - yes", pesquisa);
+  intentMap.set("agendar", agendar);  
+  agent.handleRequest(intentMap);
 
-  // If it's not empty, let's try to find the color
-  if (color) {
-    // ADD CODE FROM TODO HERE TO SAVE SUBMITTED FAVORITES
-
-    // Load our color data file
-    const colors = require("./src/colors.json");
-
-    // Take our form submission, remove whitespace, and convert to lowercase
-    color = color.toLowerCase().replace(/\s/g, "");
-
-    // Now we see if that color is a key in our colors object
-    if (colors[color]) {
-      // Found one!
-      params = {
-        color: colors[color],
-        colorError: null,
-        seo: seo,
-      };
-    } else {
-      // No luck! Return the user value as the error property
-      params = {
-        colorError: request.body.color,
-        seo: seo,
-      };
-    }
+ //FUNÇÃO BOAS VINDAS
+  function welcome(agent) {
+    if (data >= 5 && data <= 11)
+      agent.add(
+        "Olá Bom dia, seja bem-vindo(a) ao atendimento automatizado da *Barbearia Reis*" +
+          "\n" +
+          "\n" +
+          "Você já é nosso cliente?" +
+          "\n" +
+          "\n" +
+          "👉🏼 SIM \n" +
+          "👉🏼 NÃO"
+      );
+    else if (data >= 12 && data <= 17)
+      agent.add(
+        "Olá Boa tarde, seja bem-vindo(a) ao atendimento automatizado da *Barbearia Reis*" +
+          "\n" +
+          "\n" +
+          "Você já é nosso cliente?" +
+          "\n" +
+          "\n" +
+          "👉🏼 SIM \n" +
+          "👉🏼 NÃO"
+      );
+    else
+      agent.add(
+        "Olá Boa noite, seja bem-vindo(a) ao atendimento automatizado da *Barbearia Reis*" +
+          "\n" +
+          "\n" +
+          "Você já é nosso cliente?" +
+          "\n" +
+          "\n" +
+          "👉🏼 SIM \n" +
+          "👉🏼 NÃO"
+      );
   }
+  
+  
+  //AGENDAMENTO EM PLANILHA
+  
+  function agendar(agent) { 
+    const { nome, profissional, dia, hora, telefone } = agent.parameters;
+    const data = [
+      {
+        Nome: nome,
+        Servico: profissional,
+        Data: dia,
+        Horario: hora,
+        Telefone: telefone
+      }
+    ];    
+    axios.post("SUA API AQUI?sheet=agenda", data);
+    agent.add(`${nome}, o seu agendamento foi realizado com sucesso!\n` +
+    "Digite *MENU* para ver as minhas Opções.");
+  }
+  
+  
+  //CADASTRO EM PLANILHA
+  
+  function cadastro(agent) {
+    const { nome, telefone } = agent.parameters;
+    const data = [
+      {
+        Nome: nome,
+        Telefone: telefone,
+        Id: uniqueID()
+      }
+    ];    
+    axios.post("SUA API AQUI", data);
+    agent.add(`${nome}, o seu cadastro realizado com sucesso!\n` +
+    "Digite *MENU* para ver as minhas Opções.");
+  }
+  
+  //PESQUISA EM PLANILHA
 
-  // The Handlebars template will use the parameter values to update the page with the chosen color
-  return reply.view("/src/pages/index.hbs", params);
+  function pesquisa(agent) {
+    var telefone = request.body.queryResult.parameters.telefone;    
+    return axios.get("SUA API AQUI").then(res => {
+      res.data.map(coluna => {
+        if (coluna.Telefone === telefone)
+          response.json({
+            fulfillmentText:
+              "Seja bem Vindo de volta: " +
+              "\n" +
+              " *Sr.:* " +
+              coluna.Nome +
+              "\n" +
+              " Digite *MENU* para ver as minhas Opções." 
+          });
+      });
+    });
+  }
+ 
 });
 
-// Run the server and report out to the logs
-fastify.listen(
-  { port: process.env.PORT, host: "0.0.0.0" },
-  function (err, address) {
-    if (err) {
-      console.error(err);
-      process.exit(1);
-    }
-    console.log(`Your app is listening on ${address}`);
-  }
-);
+const listener = app.listen(process.env.PORT, () => {
+  console.log("Your app is listening on port " + listener.address().port);
+});
+
+////////////////////////////FIM DO CÓDIGO////////////////////////////////
